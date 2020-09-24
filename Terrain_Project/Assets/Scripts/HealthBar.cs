4c7d410ee4b47d93b42f0986ba4676ace34f019e
Danny Pires <dannypires99@gmail.com>
2020-09-24 13:04:01 -0400

    Health System built but not merged

---
+++ Terrain_Project/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill;
+    public Text healthText;
+
+    public void SetMaxHealth(float health) 
+    {
+        slider.maxValue = health;
+        slider.value = health;
+
+        fill.color = gradient.Evaluate(1f);
+        healthText.text = slider.value.ToString() + "/" + slider.maxValue.ToString();
+    }
+    public void SetHealth(float health) 
+    {
+        slider.value = health;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+
+        healthText.text = slider.value.ToString() + "/" + slider.maxValue.ToString();
+    }
+
+
+}
---
+++ Terrain_Project/Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float maxHealth = 100.0f;
+    public float health;
+    public HealthBar healthBar;
+    public int regen = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+     health = maxHealth;
+     healthBar.SetMaxHealth(maxHealth);
+     StartCoroutine(Heal());  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+                
+    }
+
+    void TakeDamage(float damage)
+    {
+        health -= damage;
+        healthBar.SetHealth(health);
+    }
+
+    IEnumerator Heal()
+    {
+        while(true)
+        {
+            if (health < maxHealth)
+            {
+                health += regen;
+                healthBar.SetHealth(health);
+            }
+        yield return new WaitForSeconds(1f);
+        }
+    }
+}
