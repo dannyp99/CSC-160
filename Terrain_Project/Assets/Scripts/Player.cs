d2666f9adb2f552229946df6efcb90e298f95b43
Danny Pires <dannypires99@gmail.com>
2020-10-27 13:50:34 -0400

    Firing the gun and killing enemies is fully functional

--- Terrain_Project/Assets/Scripts/Player.cs
+++ Terrain_Project/Assets/Scripts/Player.cs
@@ -11,13 +11,20 @@ public class Player : MonoBehaviour
 
     [SerializeField]
     private int delay = 5;
+    [SerializeField]
+    private Vector3 offset;
     private float damage = 10f;
     private float lastDamageTime = -1.0f;
+    private float fireRate = 4;
+    private float nextTimeToFire = 1f;
     public float health;
     public HealthBar healthBar;
     public GameManager manager;
     private GameObject body;
     private Camera cam;
+    public ParticleSystem leftMuzzle;
+    public ParticleSystem rightMuzzle;
+    public GameObject impactEffect;
 
     // Start is called before the first frame update
     void Start()
@@ -26,13 +33,15 @@ public class Player : MonoBehaviour
         health = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
         cam = Camera.main;
+        offset = new Vector3(0, -18, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Fire1"))
+        if(Input.GetButton("Fire2") && Time.time >= nextTimeToFire)
         {
+            nextTimeToFire = Time.time + 1f/fireRate;
             Shoot();
         }
         if(lastDamageTime >=0 && Time.time - lastDamageTime >= delay)
@@ -64,9 +73,11 @@ public class Player : MonoBehaviour
 
     void Shoot()
     {
+        leftMuzzle.Play();
+        rightMuzzle.Play();
         GameObject plyr = this.gameObject;
         Vector3 playerPos = cam.ScreenToWorldPoint(Vector3.zero);
-        Vector3 aimTarget = plyr.transform.position + plyr.transform.forward * 100;
+        Vector3 aimTarget = (plyr.transform.position + offset) + plyr.transform.forward * 100;
         Vector3 dir  = aimTarget - playerPos;
 
         UnityEngine.Debug.DrawRay(playerPos, dir, Color.yellow, .5f);
@@ -79,6 +90,8 @@ public class Player : MonoBehaviour
                 target.TakeDamage(damage);
             }
             aimTarget = hitInfo.point;
+            GameObject impactParticle = Instantiate(impactEffect, aimTarget, Quaternion.LookRotation(hitInfo.normal));
+            Destroy(impactParticle, 2f);
         }
     }
 }
